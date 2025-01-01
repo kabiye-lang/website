777fccbd8df380f79546cd18ea0c4b8f9c0d8e13
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2025-01-01 15:44:28 +0100

    fix nextjs export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    reactCompiler: true, // Enable the React compiler for better performance and compatibility with newer versions of React.
+    inlineCss: true,
+  },
+  output: "export",
 };
 
 export default nextConfig;
